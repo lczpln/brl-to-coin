6ff7fab1d382938beb60d5e4b351910960809413
Lucas Pellison <lucaspellison@gmail.com>
2019-07-04 17:11:42 -0300

    Updates

---
+++ precache-manifest.29b8e860b0b6a0fa83ccd23ec48b3906.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0f3e4667be41509ef23a7bee45aed6",
+    "url": "/brl-to-coin/index.html"
+  },
+  {
+    "revision": "f1eccb6d145d3c135f78",
+    "url": "/brl-to-coin/static/js/2.ceb9938a.chunk.js"
+  },
+  {
+    "revision": "e0290790f846e71d2a66",
+    "url": "/brl-to-coin/static/js/main.690d322a.chunk.js"
+  },
+  {
+    "revision": "360fac50bc04b4472026",
+    "url": "/brl-to-coin/static/js/runtime~main.9089ba9a.js"
+  },
+  {
+    "revision": "a45dd9193dd0aca8924cdef01138562e",
+    "url": "/brl-to-coin/static/media/coins.a45dd919.svg"
+  }
+]);
